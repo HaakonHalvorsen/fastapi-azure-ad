f2d3f25c4af0e58a0333b396891fa287978f65c7
Håkon Skogland Halvorsen <hakon@alv.no>
2024-09-19 22:08:53 +0200

    Added env example and simplified code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# fastapi-azure-ad
+# FastAPI with Azure AD autentication
 Testing Azure AD autentication and authorization for FastAPI
+
+Followed the documentation [here](https://intility.github.io/fastapi-azure-auth/).
--- main.py
+++ main.py
@@ -2,29 +2,14 @@ import uvicorn
 from fastapi import FastAPI, Security
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_azure_auth import SingleTenantAzureAuthorizationCodeBearer
-from pydantic import AnyHttpUrl, computed_field
 from pydantic_settings import BaseSettings
-from contextlib import asynccontextmanager
-from typing import AsyncGenerator
+
+SCOPE_NAME = "user_impersonation"
 
 class Settings(BaseSettings):
-    BACKEND_CORS_ORIGINS: list[str | AnyHttpUrl] = ['http://localhost:8000']
     OPENAPI_CLIENT_ID: str = ""
     APP_CLIENT_ID: str = ""
     TENANT_ID: str = ""
-    SCOPE_DESCRIPTION: str = "user_impersonation"
-
-    @computed_field
-    @property
-    def SCOPE_NAME(self) -> str:
-        return f'api://{self.APP_CLIENT_ID}/{self.SCOPE_DESCRIPTION}'
-
-    @computed_field
-    @property
-    def SCOPES(self) -> dict:
-        return {
-            self.SCOPE_NAME: self.SCOPE_DESCRIPTION,
-        }
 
     class Config:
         env_file = '.env'
@@ -33,37 +18,27 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
-@asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    """
-    Load OpenID config on startup.
-    """
-    await azure_scheme.openid_config.load_config()
-    yield
-
 app = FastAPI(
     swagger_ui_oauth2_redirect_url='/oauth2-redirect',
     swagger_ui_init_oauth={
         'usePkceWithAuthorizationCodeGrant': True,
         'clientId': settings.OPENAPI_CLIENT_ID,
+        'scopes': f'api://{settings.APP_CLIENT_ID}/{SCOPE_NAME}'
     },
 )
 
-app = FastAPI()
-
-if settings.BACKEND_CORS_ORIGINS:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-        allow_credentials=True,
-        allow_methods=['*'],
-        allow_headers=['*'],
-    )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:8000'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 azure_scheme = SingleTenantAzureAuthorizationCodeBearer(
     app_client_id=settings.APP_CLIENT_ID,
     tenant_id=settings.TENANT_ID,
-    scopes=settings.SCOPES,
+    scopes={f'api://{settings.APP_CLIENT_ID}/{SCOPE_NAME}': SCOPE_NAME},
 )
 
 @app.get("/", dependencies=[Security(azure_scheme)])
